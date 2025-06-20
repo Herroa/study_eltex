197992a4097a0a7170e470ceea8f024d0ded2f40
Herroa <roman.gordov@mail.ru>
2025-06-20 19:36:23 +0700

    general chat:

--- hw13-messageq/general_chat/client.c
+++ hw13-messageq/general_chat/client.c
@@ -1,3 +1,4 @@
+#include <ncurses.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,15 +22,21 @@ struct chatmsg {
 int q_in, q_out;
 pid_t pid;
 char name[MAX_NAME];
+WINDOW *history_win, *input_win;
+#define HISTORY_HEIGHT (LINES - 3)
+#define INPUT_HEIGHT 3
 
 void* receiver(void* arg) {
   struct chatmsg msg;
   while (1) {
     if (msgrcv(q_out, &msg, sizeof(msg) - sizeof(long), pid, 0) > 0) {
       if (msg.event == 0)
-        printf("%s: %s\n", msg.name, msg.text);
+        wprintw(history_win, "%s: %s\n", msg.name, msg.text);
       else
-        printf("* %s *\n", msg.text);
+        wprintw(history_win, "* %s *\n", msg.text);
+      wrefresh(history_win);
+      wmove(input_win, 1, 4);
+      wrefresh(input_win);
     }
   }
   return NULL;
@@ -47,12 +54,33 @@ int main() {
   struct chatmsg msg = {1, pid, "", "", 1};
   strncpy(msg.name, name, MAX_NAME);
   msgsnd(q_in, &msg, sizeof(msg) - sizeof(long), 0);
+
+  initscr();
+  cbreak();
+  noecho();
+  curs_set(1);
+  int width = COLS;
+  int height = LINES;
+  history_win = newwin(height - 3, width, 0, 0);
+  input_win = newwin(3, width, height - 3, 0);
+  scrollok(history_win, TRUE);
+  box(input_win, 0, 0);
+  wrefresh(history_win);
+  wrefresh(input_win);
+
   pthread_t recv_thread;
   pthread_create(&recv_thread, NULL, receiver, NULL);
+
+  char text[MAX_TEXT];
   while (1) {
-    char text[MAX_TEXT];
-    fgets(text, MAX_TEXT, stdin);
-    text[strcspn(text, "\n")] = 0;
+    werase(input_win);
+    box(input_win, 0, 0);
+    mvwprintw(input_win, 1, 2, "> ");
+    wmove(input_win, 1, 4);
+    wrefresh(input_win);
+    echo();
+    wgetnstr(input_win, text, MAX_TEXT - 1);
+    noecho();
     if (strcmp(text, "exit") == 0) {
       msg.event = 2;
       msgsnd(q_in, &msg, sizeof(msg) - sizeof(long), 0);
@@ -61,6 +89,9 @@ int main() {
     msg.event = 0;
     strncpy(msg.text, text, MAX_TEXT);
     msgsnd(q_in, &msg, sizeof(msg) - sizeof(long), 0);
+    wmove(input_win, 1, 4);
+    wrefresh(input_win);
   }
+  endwin();
   return 0;
 }
